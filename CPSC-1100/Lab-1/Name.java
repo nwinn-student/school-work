6263ff5b3d6557da198f70f02f0ac1c0cad551bc
Noah Winn <111249648+nwinn-student@users.noreply.github.com>
2024-05-07 00:03:55 +0000

    Add files via upload

---
+++ CPSC-1100/Lab-1/Name.java
@@ -0,0 +1,15 @@
+/**
+ * Adding a third string to the program.
+ *
+ * Noah Winn
+ * QWX746
+ * Lab1
+ * 8/26/2021
+ */
+public class Name{
+    public static void main(String[] args){
+        System.out.println("CPSC is great");
+        System.out.println("I love programming");
+        System.out.println("My name is Noah Winn");
+    }
+}
